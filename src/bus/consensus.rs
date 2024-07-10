ce7e026dcd641eb8e4fa2fb3eff882625b3abc90
Roland Rauch <roland@rauch.com>
2024-07-10 18:28:32 +0200

    feat: implemented more bus consensus functions

--- src/bus/consensus.rs
+++ src/bus/consensus.rs
@@ -1,4 +1,4 @@
-use crate::{ApiRequest, ApiRequestBuilder, ClientInner, Error};
+use crate::{ApiRequest, ApiRequestBuilder, ClientInner, Error, U128Wrapper};
 use chrono::{DateTime, FixedOffset};
 use serde::Deserialize;
 use std::sync::Arc;
@@ -21,6 +21,43 @@ impl Api {
             .json()
             .await?)
     }
+
+    pub async fn network(&self) -> Result<State, Error> {
+        Ok(self
+            .inner
+            .send_api_request(&network_req())
+            .await?
+            .json()
+            .await?)
+    }
+
+    pub async fn sia_fund_fee(&self, payout: u128) -> Result<u128, Error> {
+        let resp: U128Wrapper = self
+            .inner
+            .send_api_request(&sia_fund_fee_req(payout))
+            .await?
+            .json()
+            .await?;
+
+        Ok(resp.0)
+    }
+
+    //todo: implement `accept_block` function
+}
+
+fn sia_fund_fee_req(payout: u128) -> ApiRequest {
+    ApiRequestBuilder::get(format!("./bus/consensus/siafundfee/{}", payout)).build()
+}
+
+fn network_req() -> ApiRequest {
+    ApiRequestBuilder::get("./bus/consensus/network").build()
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all(deserialize = "camelCase"))]
+pub struct Network {
+    #[serde(rename = "Name")]
+    pub name: String,
 }
 
 fn state_req() -> ApiRequest {
@@ -38,9 +75,16 @@ pub struct State {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::RequestType;
 
     #[test]
-    fn deserialize_state() -> anyhow::Result<()> {
+    fn state() -> anyhow::Result<()> {
+        let req = state_req();
+        assert_eq!(req.path, "./bus/consensus/state");
+        assert_eq!(req.request_type, RequestType::Get);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, None);
+
         let json = r#"
         {
   "blockHeight": 436326,
@@ -56,6 +100,44 @@ mod tests {
             state.last_block_time,
             DateTime::parse_from_rfc3339("2023-09-22T14:37:32Z")?
         );
+
+        Ok(())
+    }
+
+    #[test]
+    fn network() -> anyhow::Result<()> {
+        let req = network_req();
+        assert_eq!(req.path, "./bus/consensus/network");
+        assert_eq!(req.request_type, RequestType::Get);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, None);
+
+        let json = r#"
+        {
+	"Name": "zen"
+}
+        "#;
+
+        let network: Network = serde_json::from_str(&json)?;
+        assert_eq!(network.name, "zen");
+
+        Ok(())
+    }
+
+    #[test]
+    fn sia_fund_fee() -> anyhow::Result<()> {
+        let req = sia_fund_fee_req(900000);
+        assert_eq!(req.path, "./bus/consensus/siafundfee/900000");
+        assert_eq!(req.request_type, RequestType::Get);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, None);
+
+        let json = r#"
+        "30000"
+        "#;
+
+        let resp: U128Wrapper = serde_json::from_str(&json)?;
+        assert_eq!(resp.0, 30000);
 
         Ok(())
     }
