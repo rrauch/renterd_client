f49aa5b88538aaf47e6784f2777bf9de3f3e0780
Roland Rauch <roland@rauch.com>
2024-07-11 16:35:18 +0200

    added additional bus host functions

--- src/bus/host.rs
+++ src/bus/host.rs
@@ -1,8 +1,9 @@
-use crate::{deserialize_option_string, ApiRequest, ApiRequestBuilder};
+use crate::Error::InvalidDataError;
+use crate::{deserialize_option_string, ApiRequest, ApiRequestBuilder, RequestContent};
 use crate::{ClientInner, Error, PublicKey, SettingsId};
 use bigdecimal::BigDecimal;
 use chrono::{DateTime, FixedOffset};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::num::NonZeroUsize;
 use std::sync::Arc;
@@ -30,6 +31,220 @@ impl Api {
             .json()
             .await?)
     }
+
+    pub async fn get_by_key(&self, key: &PublicKey) -> Result<Host, Error> {
+        Ok(self
+            .inner
+            .send_api_request(&get_by_key_req(key))
+            .await?
+            .json()
+            .await?)
+    }
+
+    pub async fn allowlist(&self) -> Result<Vec<PublicKey>, Error> {
+        Ok(self
+            .inner
+            .send_api_request(&allowlist_req())
+            .await?
+            .json()
+            .await?)
+    }
+
+    pub async fn modify_allowlist(&self, action: ModifyAction<PublicKey>) -> Result<(), Error> {
+        let _ = self
+            .inner
+            .send_api_request(&modify_allowlist_req(action)?)
+            .await?;
+        Ok(())
+    }
+
+    pub async fn blocklist(&self) -> Result<Vec<String>, Error> {
+        Ok(self
+            .inner
+            .send_api_request(&blocklist_req())
+            .await?
+            .json()
+            .await?)
+    }
+
+    pub async fn modify_blocklist(&self, action: ModifyAction<String>) -> Result<(), Error> {
+        let _ = self
+            .inner
+            .send_api_request(&modify_blocklist_req(action)?)
+            .await?;
+        Ok(())
+    }
+
+    //todo: implement missing `pricetables` function
+
+    pub async fn remove(
+        &self,
+        min_recent_scan_failures: u64,
+        max_downtime_hours: u64,
+        bucket: Option<String>,
+    ) -> Result<u64, Error> {
+        Ok(self
+            .inner
+            .send_api_request(&remove_req(
+                min_recent_scan_failures,
+                max_downtime_hours,
+                bucket,
+            )?)
+            .await?
+            .json()
+            .await?)
+    }
+
+    //todo: implement `scans` function
+
+    pub async fn scanning(
+        &self,
+        offset: Option<NonZeroUsize>,
+        limit: Option<NonZeroUsize>,
+        last_scan: Option<DateTime<FixedOffset>>,
+    ) -> Result<Vec<HostAddress>, Error> {
+        Ok(self
+            .inner
+            .send_api_request(&scanning_req(offset, limit, last_scan))
+            .await?
+            .json()
+            .await?)
+    }
+
+    pub async fn reset_lost_sectors(&self, key: &PublicKey) -> Result<(), Error> {
+        let _ = self
+            .inner
+            .send_api_request(&reset_lost_sectors_req(key))
+            .await?;
+        Ok(())
+    }
+}
+
+fn reset_lost_sectors_req(key: &PublicKey) -> ApiRequest {
+    ApiRequestBuilder::post(format!("./bus/host/{}/resetlostsectors", key)).build()
+}
+
+fn get_by_key_req(key: &PublicKey) -> ApiRequest {
+    ApiRequestBuilder::get(format!("./bus/host/{}", key)).build()
+}
+
+fn scanning_req(
+    offset: Option<NonZeroUsize>,
+    limit: Option<NonZeroUsize>,
+    last_scan: Option<DateTime<FixedOffset>>,
+) -> ApiRequest {
+    let mut params = Vec::with_capacity(3);
+    if let Some(offset) = offset {
+        params.push(("offset", offset.to_string()));
+    }
+    if let Some(limit) = limit {
+        params.push(("limit", limit.to_string()));
+    }
+    if let Some(last_scan) = last_scan {
+        params.push(("lastScan", last_scan.to_rfc3339()));
+    }
+    let params = if params.is_empty() {
+        None
+    } else {
+        Some(params)
+    };
+    ApiRequestBuilder::get("./bus/hosts/scanning")
+        .params(params)
+        .build()
+}
+
+#[derive(Deserialize, Clone, Debug, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct HostAddress {
+    pub public_key: PublicKey,
+    pub net_address: String,
+}
+
+fn remove_req(
+    min_recent_scan_failures: u64,
+    max_downtime_hours: u64,
+    bucket: Option<String>,
+) -> Result<ApiRequest, Error> {
+    let params = bucket.map(|b| vec![("bucket", b)]);
+    let content = Some(RequestContent::Json(
+        serde_json::to_value(RemoveRequest {
+            min_recent_scan_failures,
+            max_downtime_hours,
+        })
+        .map_err(|e| InvalidDataError(e.into()))?,
+    ));
+    Ok(ApiRequestBuilder::post("./bus/hosts/remove")
+        .content(content)
+        .params(params)
+        .build())
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct RemoveRequest {
+    min_recent_scan_failures: u64,
+    max_downtime_hours: u64,
+}
+
+fn blocklist_req() -> ApiRequest {
+    ApiRequestBuilder::get("./bus/hosts/blocklist").build()
+}
+
+fn modify_blocklist_req(action: ModifyAction<String>) -> Result<ApiRequest, Error> {
+    let req: ModifyRequest<String> = action.into();
+    let content = Some(RequestContent::Json(
+        serde_json::to_value(req).map_err(|e| InvalidDataError(e.into()))?,
+    ));
+    Ok(ApiRequestBuilder::put("./bus/hosts/blocklist")
+        .content(content)
+        .build())
+}
+
+fn modify_allowlist_req(action: ModifyAction<PublicKey>) -> Result<ApiRequest, Error> {
+    let req: ModifyRequest<PublicKey> = action.into();
+    let content = Some(RequestContent::Json(
+        serde_json::to_value(req).map_err(|e| InvalidDataError(e.into()))?,
+    ));
+    Ok(ApiRequestBuilder::put("./bus/hosts/allowlist")
+        .content(content)
+        .build())
+}
+
+pub enum ModifyAction<T: Serialize> {
+    AddRemove {
+        add: Option<Vec<T>>,
+        remove: Option<Vec<T>>,
+    },
+    Clear,
+}
+
+impl<T: Serialize> From<ModifyAction<T>> for ModifyRequest<T> {
+    fn from(value: ModifyAction<T>) -> Self {
+        match value {
+            ModifyAction::AddRemove { add, remove } => ModifyRequest {
+                add: add.unwrap_or(vec![]),
+                remove: remove.unwrap_or(vec![]),
+                clear: false,
+            },
+            ModifyAction::Clear => ModifyRequest {
+                add: vec![],
+                remove: vec![],
+                clear: true,
+            },
+        }
+    }
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct ModifyRequest<T: Serialize> {
+    add: Vec<T>,
+    remove: Vec<T>,
+    clear: bool,
+}
+
+fn allowlist_req() -> ApiRequest {
+    ApiRequestBuilder::get("./bus/hosts/allowlist").build()
 }
 
 fn list_req(offset: Option<NonZeroUsize>, limit: Option<NonZeroUsize>) -> ApiRequest {
@@ -301,6 +516,7 @@ pub struct UsabilityBreakDown {
 mod tests {
     use super::*;
     use crate::RequestType;
+    use serde_json::Value;
 
     #[test]
     fn list() -> anyhow::Result<()> {
@@ -840,6 +1056,327 @@ mod tests {
         assert_eq!(checks.usability.gouging, true);
         assert_eq!(checks.usability.not_accepting_contracts, false);
 
+        Ok(())
+    }
+
+    #[test]
+    fn allowlist() -> anyhow::Result<()> {
+        let req = allowlist_req();
+        assert_eq!(req.path, "./bus/hosts/allowlist");
+        assert_eq!(req.request_type, RequestType::Get);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, None);
+
+        let json = r#"
+["ed25519:6f7ac63891fa2eadeb3031b75817a4beaae91070f485c3d139f1ffd3107d6aa8"]
+        "#;
+
+        let resp: Vec<PublicKey> = serde_json::from_str(&json)?;
+        assert_eq!(resp.len(), 1);
+        assert_eq!(
+            resp.get(0).unwrap(),
+            &"ed25519:6f7ac63891fa2eadeb3031b75817a4beaae91070f485c3d139f1ffd3107d6aa8"
+                .try_into()?
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn modify_allowlist() -> anyhow::Result<()> {
+        let json = r#"
+        {
+    "add": [],
+    "remove": ["ed25519:6f7ac63891fa2eadeb3031b75817a4beaae91070f485c3d139f1ffd3107d6aa8"],
+    "clear": false
+}
+        "#;
+        let expected: Value = serde_json::from_str(&json)?;
+
+        let req = modify_allowlist_req(ModifyAction::AddRemove {
+            add: None,
+            remove: Some(vec![
+                "ed25519:6f7ac63891fa2eadeb3031b75817a4beaae91070f485c3d139f1ffd3107d6aa8"
+                    .try_into()?,
+            ]),
+        })?;
+        assert_eq!(req.path, "./bus/hosts/allowlist");
+        assert_eq!(req.request_type, RequestType::Put);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, Some(RequestContent::Json(expected)));
+
+        Ok(())
+    }
+
+    #[test]
+    fn blocklist() -> anyhow::Result<()> {
+        let req = blocklist_req();
+        assert_eq!(req.path, "./bus/hosts/blocklist");
+        assert_eq!(req.request_type, RequestType::Get);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, None);
+
+        let json = r#"
+["siacentral.ddnsfree.com","siacentral.mooo.com"]
+        "#;
+
+        let resp: Vec<String> = serde_json::from_str(&json)?;
+        assert_eq!(resp.len(), 2);
+        assert_eq!(resp.get(0).unwrap(), "siacentral.ddnsfree.com");
+        assert_eq!(resp.get(1).unwrap(), "siacentral.mooo.com");
+
+        Ok(())
+    }
+
+    #[test]
+    fn modify_blocklist() -> anyhow::Result<()> {
+        let json = r#"
+        {
+    "add": [],
+    "remove": ["siacentral.ddnsfree.com","siacentral.mooo.com","51.158.108.244", "45.148.30.56"],
+    "clear": false
+}
+        "#;
+        let expected: Value = serde_json::from_str(&json)?;
+
+        let req = modify_blocklist_req(ModifyAction::AddRemove {
+            add: None,
+            remove: Some(vec![
+                "siacentral.ddnsfree.com".to_string(),
+                "siacentral.mooo.com".to_string(),
+                "51.158.108.244".to_string(),
+                "45.148.30.56".to_string(),
+            ]),
+        })?;
+        assert_eq!(req.path, "./bus/hosts/blocklist");
+        assert_eq!(req.request_type, RequestType::Put);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, Some(RequestContent::Json(expected)));
+
+        Ok(())
+    }
+
+    #[test]
+    fn remove() -> anyhow::Result<()> {
+        let json = r#"
+        {
+    "minRecentScanFailures": 3,
+    "maxDowntimeHours": 1000
+}
+        "#;
+        let expected: Value = serde_json::from_str(&json)?;
+
+        let req = remove_req(3, 1000, Some("foo".to_string()))?;
+        assert_eq!(req.path, "./bus/hosts/remove");
+        assert_eq!(req.request_type, RequestType::Post);
+        assert_eq!(req.params, Some(vec![("bucket".into(), "foo".into())]));
+        assert_eq!(req.content, Some(RequestContent::Json(expected)));
+
+        let json = "0";
+        let resp: u64 = serde_json::from_str(&json)?;
+        assert_eq!(resp, 0);
+
+        Ok(())
+    }
+
+    #[test]
+    fn scanning() -> anyhow::Result<()> {
+        let req = scanning_req(
+            None,
+            Some(10.try_into()?),
+            Some(DateTime::parse_from_rfc3339("2023-03-30T15:12:15+00:00")?),
+        );
+        assert_eq!(req.path, "./bus/hosts/scanning");
+        assert_eq!(req.request_type, RequestType::Get);
+        assert_eq!(
+            req.params,
+            Some(vec![
+                ("limit".into(), "10".into()),
+                ("lastScan".into(), "2023-03-30T15:12:15+00:00".into())
+            ])
+        );
+        assert_eq!(req.content, None);
+
+        let json = "0";
+        let resp: u64 = serde_json::from_str(&json)?;
+        assert_eq!(resp, 0);
+
+        let json = r#"
+        [
+  {
+    "publicKey": "ed25519:de9e1fd0e7c19b23ac2271a3a4bceed161108d16ab708922c4573cf53fa82dfa",
+    "netAddress": "87.255.6.177:9982"
+  }
+]
+        "#;
+
+        let resp: Vec<HostAddress> = serde_json::from_str(&json)?;
+        assert_eq!(resp.len(), 1);
+        assert_eq!(
+            resp.get(0).unwrap().public_key,
+            "ed25519:de9e1fd0e7c19b23ac2271a3a4bceed161108d16ab708922c4573cf53fa82dfa"
+                .try_into()?
+        );
+        assert_eq!(resp.get(0).unwrap().net_address, "87.255.6.177:9982");
+
+        Ok(())
+    }
+
+    #[test]
+    fn get_by_key() -> anyhow::Result<()> {
+        let req = get_by_key_req(
+            &"ed25519:b050c0c63f9f3b4d5a89acadf628e8d8c6f8768e38fbe731e429334e0fd2cece"
+                .try_into()?,
+        );
+        assert_eq!(
+            req.path,
+            "./bus/host/ed25519:b050c0c63f9f3b4d5a89acadf628e8d8c6f8768e38fbe731e429334e0fd2cece"
+        );
+        assert_eq!(req.request_type, RequestType::Get);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, None);
+
+        let json = "0";
+        let resp: u64 = serde_json::from_str(&json)?;
+        assert_eq!(resp, 0);
+
+        let json = r#"
+        {
+    "knownSince": "2023-02-20T19:15:26.84+01:00",
+    "lastAnnouncement": "2024-03-01T13:57:41Z",
+    "publicKey": "ed25519:e14888420f7df8001990283b44e469e922c0dc14dc2d0156a31fbb6524d08008",
+    "netAddress": "shawnhomesc.no-ip.org:9982",
+    "priceTable": {
+      "uid": "daab22a125d9fa4ffbfb099cbba2e06b",
+      "validity": 600000000000,
+      "hostblockheight": 410818,
+      "updatepricetablecost": "1",
+      "accountbalancecost": "1",
+      "fundaccountcost": "1",
+      "latestrevisioncost": "253600000000000000",
+      "subscriptionmemorycost": "1",
+      "subscriptionnotificationcost": "1",
+      "initbasecost": "0",
+      "memorytimecost": "1",
+      "downloadbandwidthcost": "75000000000000",
+      "uploadbandwidthcost": "150000000000000",
+      "dropsectorsbasecost": "1",
+      "dropsectorsunitcost": "1",
+      "hassectorbasecost": "1",
+      "readbasecost": "0",
+      "readlengthcost": "1",
+      "renewcontractcost": "100000000000000000",
+      "revisionbasecost": "0",
+      "swapsectorcost": "1",
+      "writebasecost": "0",
+      "writelengthcost": "1",
+      "writestorecost": "92592592592",
+      "txnfeeminrecommended": "10000000000000000000",
+      "txnfeemaxrecommended": "30000000000000000000",
+      "contractprice": "150000000000000000000000",
+      "collateralcost": "23148148148",
+      "maxcollateral": "2500000000000000000000000000",
+      "maxduration": 27216,
+      "windowsize": 144,
+      "registryentriesleft": 15578716,
+      "registryentriestotal": 15625024,
+      "expiry": "2024-07-04T12:19:01.025014279Z"
+    },
+    "settings": {
+      "acceptingcontracts": true,
+      "baserpcprice": "0",
+      "collateral": "23148148148",
+      "contractprice": "150000000000000000000000",
+      "downloadbandwidthprice": "75000000000000",
+      "ephemeralaccountexpiry": 604800000000000,
+      "maxcollateral": "2500000000000000000000000000",
+      "maxdownloadbatchsize": 17825792,
+      "maxduration": 27216,
+      "maxephemeralaccountbalance": "1000000000000000000000000",
+      "maxrevisebatchsize": 17825792,
+      "netaddress": "shawnhomesc.no-ip.org:9982",
+      "remainingstorage": 2051270508544,
+      "revisionnumber": 35680855,
+      "sectoraccessprice": "0",
+      "sectorsize": 4194304,
+      "siamuxport": "9983",
+      "storageprice": "92592592592",
+      "totalstorage": 4748354781184,
+      "unlockhash": "326c36421b0ad2deee9f54fa98d1829e2a917cfc9febeeaa4f20b9121b434a7e8dad6b2f9578",
+      "uploadbandwidthprice": "150000000000000",
+      "version": "1.5.9",
+      "release": "hostd 58db87c",
+      "windowsize": 144
+    },
+    "interactions": {
+      "totalScans": 36,
+      "lastScan": "2023-03-29T15:42:35.974329629+02:00",
+      "lastScanSuccess": true,
+      "lostSectors": 0,
+      "secondToLastScanSuccess": true,
+      "uptime": 3072358865133168,
+      "downtime": 90631369071825,
+      "successfulInteractions": 207,
+      "failedInteractions": 21
+    },
+    "scanned": true,
+    "blocked": false,
+    "checks": {
+      "autopilot": {
+        "gouging": {
+          "contractErr": "",
+          "downloadErr": "",
+          "gougingErr": "rpc price too high, 100 nS \u003e 40 nS",
+          "pruneErr": "",
+          "uploadErr": ""
+        },
+        "score": {
+          "age": 0,
+          "collateral": 0,
+          "interactions": 0,
+          "storageRemaining": 0,
+          "uptime": 0,
+          "version": 0,
+          "prices": 0
+        },
+        "usability": {
+          "blocked": false,
+          "offline": false,
+          "lowScore": false,
+          "redundantIP": false,
+          "gouging": true,
+          "notAcceptingContracts": false,
+          "notAnnounced": false,
+          "notCompletingScan": false
+        }
+      }
+    },
+    "storedData": 0
+  }
+        "#;
+
+        let resp: Host = serde_json::from_str(&json)?;
+        assert_eq!(
+            resp.public_key,
+            "ed25519:e14888420f7df8001990283b44e469e922c0dc14dc2d0156a31fbb6524d08008"
+                .try_into()?
+        );
+        assert_eq!(resp.net_address, "shawnhomesc.no-ip.org:9982");
+        assert_eq!(resp.stored_data, 0);
+
+        Ok(())
+    }
+
+    #[test]
+    fn reset_lost_sectors() -> anyhow::Result<()> {
+        let req = reset_lost_sectors_req(
+            &"ed25519:5150ae4bed4a2da68211243ded72a6fc166c860560023fd6f7221e54f5f478da"
+                .try_into()?,
+        );
+        assert_eq!(req.path, "./bus/host/ed25519:5150ae4bed4a2da68211243ded72a6fc166c860560023fd6f7221e54f5f478da/resetlostsectors");
+        assert_eq!(req.request_type, RequestType::Post);
+        assert_eq!(req.params, None);
+        assert_eq!(req.content, None);
         Ok(())
     }
 }
